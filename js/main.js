701e1ac412a8a2f38586f526fc8ad0079188f771
nikolay <kolyapavlov7@yandex.ru>
2020-11-12 00:09:53 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
-// alert('Колян петух');
 var CANVAS_HEIGHT = 800;
 var CANVAS_WIDTH = 1200;
 
-var PLAYER_BOTTOM = 100;
-var FOOT_LENGTH = 30;
-var HAND_LENGTH = 20;
-var HEAD_SIZE = 30;
 
-var coords1 = {
-    height: 50,
-    left: 100
-};
-
-var coords2 = {
-    height: 35,
-    left: 200
-};
     
 var GAME = {
     init: function() {
-        var canvas = document.getElementById("canvas");
-        this.ctx = canvas.getContext("2d");
+        this.setupPlayersChoice();
+        this.setupCanvas();
         this.setupField();
-        this.setupPlayer1();
-        this.setupPlayer2();
+    },
+
+    setupPlayersChoice: function() {
+        var self = this;
+        self.$players = $('#players');
+        self.$players.find('.player').on('click', function(e) {
+            var player = $(e.target).attr('id');
+            if (self.player1 === undefined) {
+                self.setupPlayer1(player);
+            } else if (self.player1 && self.player2 === undefined) {
+                self.setupPlayer2(player);
+                self.hidePlayersChoice();
+            }
+        });
+    },
+
+    hidePlayersChoice: function() {
+        this.$players.addClass('d-none');
+    },
+
+    setupCanvas: function() {
+        var canvas = $("#canvas")[0];
+        this.ctx = canvas.getContext("2d");
     },
     
     setupField: function() {
@@ -31,41 +38,18 @@ var GAME = {
         this.ctx.fillRect(10, 10, CANVAS_HEIGHT, CANVAS_WIDTH);
     },
     
-    setupPlayer1: function() {
-        this.setupPlayer(coords1, 'kolyan');
+    setupPlayer1: function(player) {
+        $('#player1').text(player);
+        this.player1 = Player;
+        this.player1.init(this.ctx);
+        this.player1.create(coords1, player);
     },
 
-    setupPlayer2: function() {
-        this.setupPlayer(coords2, 'azamat');
-    },
-    
-    setupPlayer: function(coords, player_name) {
-        this.ctx.beginPath(); 
-        var top = PLAYER_BOTTOM - coords.height;
-
-        // тело
-        this.ctx.moveTo(coords.left, top);
-        this.ctx.lineTo(coords.left, PLAYER_BOTTOM);
-
-        // ноги
-        this.ctx.moveTo(coords.left, PLAYER_BOTTOM);
-        this.ctx.lineTo(coords.left-FOOT_LENGTH, PLAYER_BOTTOM+FOOT_LENGTH);
-        this.ctx.moveTo(coords.left, PLAYER_BOTTOM);
-        this.ctx.lineTo(coords.left+FOOT_LENGTH, PLAYER_BOTTOM+FOOT_LENGTH);
-
-        // руки
-        var ple40 = top+10;
-        this.ctx.moveTo(coords.left, ple40);
-        this.ctx.lineTo(coords.left-HAND_LENGTH, ple40+HAND_LENGTH);
-        this.ctx.moveTo(coords.left, ple40);
-        this.ctx.lineTo(coords.left+HAND_LENGTH, ple40+HAND_LENGTH);
-
-        // голова
-        var img = document.getElementById(player_name);
-        var half_head_size = HEAD_SIZE / 2;
-        this.ctx.drawImage(img, coords.left-half_head_size, top-HEAD_SIZE, HEAD_SIZE, HEAD_SIZE);
-
-        this.ctx.stroke();
+    setupPlayer2: function(player) {
+        $('#player2').text(player);
+        this.player2 = Player;
+        this.player2.init(this.ctx);
+        this.player2.create(coords2, player);
     },
     
 }
--- js/player.js
+++ js/player.js
@@ -1,3 +1,59 @@
-Player: function() {
+var PLAYER_BOTTOM = 100;
+var FOOT_LENGTH = 30;
+var HAND_LENGTH = 20;
+var HEAD_SIZE = 30;
 
+var PLAYER_HEIGHTS = {
+    haramarazmat: 35,
+    mozgovoi: 50
+}
+
+var coords1 = {
+    left: 100
+};
+
+var coords2 = {
+    left: 200
+};
+
+var Player = {
+    init: function(ctx) {
+        this.ctx = ctx;
+    },
+
+    create: function(coords, player) {
+        this.player = player;
+        this.ctx.beginPath();
+        var top = PLAYER_BOTTOM - PLAYER_HEIGHTS[this.player];
+
+        // тело
+        this.ctx.moveTo(coords.left, top);
+        this.ctx.lineTo(coords.left, PLAYER_BOTTOM);
+
+        // ноги
+        this.ctx.moveTo(coords.left, PLAYER_BOTTOM);
+        this.ctx.lineTo(coords.left-FOOT_LENGTH, PLAYER_BOTTOM+FOOT_LENGTH);
+        this.ctx.moveTo(coords.left, PLAYER_BOTTOM);
+        this.ctx.lineTo(coords.left+FOOT_LENGTH, PLAYER_BOTTOM+FOOT_LENGTH);
+
+        // руки
+        var ple40 = top+10; // плечо)))
+        this.ctx.moveTo(coords.left, ple40);
+        this.ctx.lineTo(coords.left-HAND_LENGTH, ple40+HAND_LENGTH);
+        this.ctx.moveTo(coords.left, ple40);
+        this.ctx.lineTo(coords.left+HAND_LENGTH, ple40+HAND_LENGTH);
+
+        // голова
+        var img = document.getElementById(player);
+        var half_head_size = HEAD_SIZE / 2;
+        this.ctx.drawImage(img, coords.left-half_head_size, top-HEAD_SIZE, HEAD_SIZE, HEAD_SIZE);
+
+        this.ctx.stroke();
+    },
+
+    handKick: function() {
+        // todo: короч остановился на том, что хз как нарисовать удар
+        //  я думал, что можно как-то изменить координаты руки
+        //  но хуй там плавал, походу для каждого движения нужно будет перерисовывать канву ебучую
+    },
 }
